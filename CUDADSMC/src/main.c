95c94a50a5321b83c2007a297ab08feb4f33e903
Chris Watkins (macbook) <christopher.watkins@me.com>
2014-08-01 09:13:26 +1000

    first commit

---
+++ CUDADSMC/src/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CUDADSMC
+//
+//  Created by Christopher Watkins on 31/07/2014.
+//  Copyright (c) 2014 WIJ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
---
+++ CUDADSMC/src/moveAtoms.cu
@@ -0,0 +1,16 @@
+//
+//  moveAtoms.cu
+//  CUDADSMC
+//
+//  Created by Christopher Watkins on 31/07/2014.
+//  Copyright (c) 2014 WIJ. All rights reserved.
+//
+
+#include <stdio.h>
+
+__global__ void moveAtoms( double4 *pos, double4 *vel, int numberOfAtoms ) {
+    
+    
+    
+    return;
+}
