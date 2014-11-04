Homogeneous gas, looks good. I may have to fix a factor of root 2 in the analytic collision rate in the other branches.

CUDADSMC/collisionChecker.py
@@ -24,13 +24,15 @@ dBdr = 8746.;
 
 tres = 151;
 nAtoms = 1e6;
-nCells = 100**3 + 1;
+nCells = 10**3 + 1;
 
 vtherm = np.sqrt( 8.*kB*T / (pi*mRb) );
+vrel = np.sqrt(2) * vtherm
 sigma = 8. * pi * a**2;
-Ve = 2.*np.sqrt(2.)*pi**1.5 / ((dBdr*gs*muB / kB / T)**1.5)
+#Ve = (2. * 0.00005)**3
+Ve = (2. * 0.00005)**3
 n0 = nAtoms / Ve
-collisionRate = 0.5 * n0 * sigma * vtherm * (1. / (2.*np.sqrt(2.)) )
+collisionRate = 0.5 * n0 * sigma * vrel
 
 print "Analytic collision rate = ", collisionRate
 

CUDADSMC/energyChecker.py
@@ -22,7 +22,7 @@ hbar = 1.05457148e-34;
 T    = 20.e-6;
 dBdr = 8746.;
 
-tres = 151;
+tres = 26;
 ntrials = 1e4;
 dt = 1e-6;
 
@@ -51,7 +51,7 @@ dset.read_direct(N);
 
 f.close()
 
-time = time * 1.81;
+time = time * 28.3;
 
 Ek = np.zeros((N.size,))
 Ep = np.zeros((N.size,))